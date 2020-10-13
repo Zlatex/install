5c4b2cd1b2f67984cd7566057cc010342b6a6e9e
Zlatex <maximzlatogorsky@gmail.com>
2020-10-13 15:35:52 +0300

    Update zlatex.sh

--- zlatex.sh
+++ zlatex.sh
@@ -16,8 +16,10 @@ sudo apt-get -y install wget
 sudo ./lamp.sh --apache_option 1 --db_option 3 --php_option 6 --db_manage_modules phpmyadmin,adminer --kodexplorer_option 1
 cd /usr/local/bin && sudo wget -O virtualhost https://raw.githubusercontent.com/RoverWire/virtualhost/master/virtualhost.sh && sudo chmod +x virtualhost
 sudo snap install opera && sudo snap install telegram-desktop && sudo snap install code && sudo snap install spotify 
-cd ~ && sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb
+cd ~ && sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo timedatectl set-timezone Europe/Kiev
+sudo apt install ubuntu-desktop
 clear
-echo "Вроде как установил все, сейчас попробую удалить то что понагружал"
-sudo rm -rf ~/install/*
+echo "Seccess"
+
