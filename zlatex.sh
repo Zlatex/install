f6e6b71fc263fc0469e7519644290dbe552b5419
Zlatex <maximzlatogorsky@gmail.com>
2020-10-13 09:36:28 +0300

    Update zlatex.sh

--- zlatex.sh
+++ zlatex.sh
@@ -10,8 +10,9 @@ sleep 2s
 sudo apt update
 git clone https://github.com/teddysun/lamp.git
 cd lamp
-chmod 755 *.sh ./
-sudo ./lamp.sh --apache_option 1 --apache_modules mod_wsgi,mod_security --db_option 2 --db_root_pwd teddysun.com --php_option 5 --php_extensions apcu,ioncube,imagick,redis,mongodb,libsodium,swoole --db_manage_modules phpmyadmin,adminer --kodexplorer_option 1
+chmod 755 *.sh
+sudo apt-get -y install wget
+sudo ./lamp.sh --apache_option 1 --db_option 3 --php_option 6 --kodexplorer_option 2
 cd /usr/local/bin && sudo wget -O virtualhost https://raw.githubusercontent.com/RoverWire/virtualhost/master/virtualhost.sh && sudo chmod +x virtualhost
 sudo snap install opera
 sudo snap install telegram-desktop
