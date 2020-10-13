d8c21f4cba8af3381a6ccf2158583ea2408a6ccd
Zlatex <maximzlatogorsky@gmail.com>
2020-10-13 14:17:29 +0300

    Update zlatex.sh

--- zlatex.sh
+++ zlatex.sh
@@ -1,3 +1,4 @@
+clear
 echo
 echo "		███████╗██╗░░░░░░█████╗░████████╗███████╗██╗░░██╗"
 echo "		╚════██║██║░░░░░██╔══██╗╚══██╔══╝██╔════╝╚██╗██╔╝"
@@ -14,5 +15,4 @@ chmod 755 *.sh
 sudo apt-get -y install wget
 sudo ./lamp.sh --apache_option 1 --db_option 3 --php_option 6 --db_manage_modules phpmyadmin,adminer --kodexplorer_option 1
 cd /usr/local/bin && sudo wget -O virtualhost https://raw.githubusercontent.com/RoverWire/virtualhost/master/virtualhost.sh && sudo chmod +x virtualhost
-sudo snap install opera
-sudo snap install telegram-desktop
+sudo snap install opera && sudo snap install telegram-desktop && sudo snap install code
